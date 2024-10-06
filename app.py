1b5259e4401f39ef26b66960c2bbad987b307771
Maca Sanhueza <mimobakerycl@gmail.com>
2024-10-06 10:39:26 -0300

    edit app

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ car_data = pd.read_csv("vehicles_us.csv")  # leer los datos
 st.header('Visualizaciones de performance de anuncios de venta de coches')
 
 hist_button = st.button('Construir histograma') 
+build_histogram = st.checkbox('Construir un histograma')
 disp_button = st.button('Construir gráfico de dispersión')# crear un botón
 
 if hist_button:  # al hacer clic en el botón
@@ -18,6 +19,18 @@ if hist_button:  # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+ 
+if build_histogram:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)   
+    
 
 if disp_button:  # al hacer clic en el botón
     # escribir un mensaje
